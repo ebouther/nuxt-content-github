8bd1b2569efa1da215eb977414f11a3ba14e097e
ebouther <ebouther@student.42.fr>
2021-05-31 00:21:55 +0200

    Reenabled file edits CI

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,13 +2,12 @@ export default {
 
   publicRuntimeConfig: {
     clientId: 'Iv1.f05a0dce5c26e79f',
-    clientSecret: 'e12db86eb2a555f996cad2bf5380d69923f9d8da',
     appId: '116023',
     installationId: '17141426',
     privateKey: process.env.G_APP_PRIVATE_KEY, // TODO
     owner: 'nuxt-content-github',
     repo: 'nuxt-content-github.github.io',
-    branch: 'markdown',
+    branch: 'main',
   },
   privateRuntimeConfig: {},
   /*
@@ -41,6 +40,20 @@ export default {
     '@nuxtjs/axios',
     //'@nuxtjs/auth-next'
   ],
+  hooks: {
+    'content:options': (options) => {
+      console.log('Content options:', options)
+    },
+    'file:beforeInsert': (item) => {
+      console.log('BEFORE INSERT :', item)
+    },
+    'file:beforeParse': (file) => {
+      console.log('BEFORE PARSE :', file)
+    },
+    'file:udpated': (ev) => {
+      console.log('FILE UPDATED :', ev)
+    },
+  },
   // content: {
   //   // Disable for security reason on CodeSandBox
   //   //liveEdit: false
